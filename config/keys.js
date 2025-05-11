9d6e34708c2064e8f97197fe02cf6236fa8076b8
Hebristean Alex Mircea <80382963+alexhebri@users.noreply.github.com>
2025-05-11 16:15:51 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-   mongoURI: 'mongodb+srv://hebristeanalex:itY56jCvk50N10Kq@cluster0.vug9x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+   mongoURI: 'UURIIII',
    secretOrKey: 'secret'
 };
